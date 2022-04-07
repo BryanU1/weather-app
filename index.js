d6cf5136d40f63884fc32079e1741b72dc7433ef
BryanU <ungbryan01@gmail.com>
2022-04-07 16:02:35 -0700

    added unit conversion

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 async function getWeatherData(city) {
   city = city.replace(' ', '+');
   const apiKey = '174b71bd2afaabb6bd0b45076a49b6cb';
-  let weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+  let weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
 
   try {
     const response = await fetch(weatherURL, {mode: 'cors'});
@@ -13,4 +13,8 @@ async function getWeatherData(city) {
   }
 }
 
-getWeatherData('los angeles');
+function kelvinToCelsius(k) {
+  return k - 273.15;
+}
+
+getWeatherData('san diego');
